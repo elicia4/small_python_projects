15043696cdca609d88d436765439caecd29f6f31
elicia44 <49814064+elicia4@users.noreply.github.com>
2024-10-08 13:56:08 +0200

    cleaned up job requirement sorter a little bit

--- requirements_sorter.py
+++ requirements_sorter.py
@@ -2,48 +2,52 @@
 # After the user is finished, it sorts them and the list is printed to a
 # "sorted_requirements.txt" file.
 
+# printed sorted requirements
 def print_requirements(dictionary):
     for i, tech in enumerate(dictionary):
-        print(f"{i}: {tech} {dictionary[tech]}")
+        print(f"{i + 1}: {tech} {dictionary[tech]}")
 
 def get_sorted_techs():
     # define an empty dictionary of job requirements
-    requirements = {
-    }
+    requirements = { }
     # the current technology variable
-    technology = input(f"\nPlease enter a needed technology: ").lower()
-    print("Enter \"done\" when you're done with your entries.")
-    # enter new technologies until the user inputs done
-    while (technology != "done"):
+    technology = input(f"\n> Please enter a needed technology ('q' to quit): ").lower()
+    # enter new technologies until the user inputs q
+    while (technology != "q"):
+        # if technology not in requirements, initialize it with 1
         if technology not in requirements.keys():
             requirements[technology] = 1
             print("Adding new technology...") 
+        # else increment the number of mentions
         else:
             requirements[technology] += 1
+        # after each prompt, print
+        requirements = dict(sorted(requirements.items(), key=lambda item: item[1], reverse=True))
         print_requirements(requirements)
-        technology = input(f"\nPlease enter a needed technology: ").lower()
-        print("Enter \"done\" when you're done with your entries.")
-    return dict(sorted(requirements.items(), key=lambda x:x[1], reverse=True))
+        technology = input(f"\n> Please enter a needed technology ('q' to quit): ").lower()
+    if requirements:
+        return requirements
+    else:
+        print("The list is empty. Quitting...")
+        quit()
 
-print(f"""
-######################################################################
+print(f"""######################################################################
 The purpose of this program is to help you find what things you should
 prioritize studying while applying for a job.
-######################################################################
-""")
+######################################################################""")
 
-sorted_requirements = get_sorted_techs()
+requirements = get_sorted_techs()
 
 print(f"""
 #############################################
 Here's the final sorted list of requirements:
 #############################################""")
 
-print_requirements(sorted_requirements)
+print_requirements(requirements)
 
 # write the final result to the sorted_requirements.txt file
 with open("sorted_requirements.txt", "w") as file:
-    for key in sorted_requirements.keys():
-        print(f"{key} {sorted_requirements[key]}", file=file)
+    for key in requirements.keys():
+        print(f"{key} {requirements[key]}", file=file)
 print("""#############################################\n
 Printed the sorted list to the \"sorted_requirements.txt\" file.""")
